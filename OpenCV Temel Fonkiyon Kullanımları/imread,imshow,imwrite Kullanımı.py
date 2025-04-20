e8622110ed9a8f0d8e597cd119f82412e3fde3ee
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-04-20 04:51:59 +0300

    Belli fonkisyonların kullanımı

---
+++ OpenCV Temel Fonkiyon Kullanımları/imread,imshow,imwrite Kullanımı.py
@@ -0,0 +1,26 @@
+import cv2
+import time
+"""
+Opencv ile görseli içe aktarmak için imread fonkiyonu kullanılır.
+"""
+url=r"C:\Users\Furkan\Downloads\a.jpg"
+img = cv2.imread(url ,0 )
+
+"""
+url , 0 şeklinde yazılan şey aslında resmi siyah beyaz şekilde gösterilmesini sağlar
+"""
+
+#Görselleşitirmek için ise imshow fonksiyonu kullanılır.
+cv2.imshow("resim" , img)
+
+bekle = cv2.waitKey(0) &0xFF
+if bekle == 27: #27 tuşu klavyede esc tuşu anlamına gelir
+    cv2.destroyWindow("resim")
+elif bekle == ord("a"):
+    cv2.imwrite("resim" ,img)
+
+"""
+imread: Görüntüyü okumak için kullanılır.
+imwrite: Bellekte bir görüntüyü diske yazdırır.
+imshow: namedWindow ve waitKey ile birlikte bellekteki bir resmi görüntülemek için kullanılır.
+"""
