44727d0aecaff5c142366baeb3e5a525876e61e0
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-10 21:31:49 +0300

    1

---
+++ Nesne Algılama/0.1 Nesne Tespiti Mantığı.py
@@ -0,0 +1,27 @@
+#OpenCV ile Nesne Tespiti
+"""
+Nesne algılama, dijital görüntüde ve videolarda belirli bir sınıftaki
+(insanlar, binalar veya arabalar gibi) anlamsal nesnelerin örneklerini algılamakla ilgilenen,
+bilgisayarla görme ve görüntü işleme ile ilgili bir bilgisayar teknolojisidir.
+
+Amaç görüntü üzerinde bulunan nesnenin koordinatlarını, genişlik ve yükseklik
+değerlerini bulunmasıdır.
+
+Yani yanlış anlama olmaması adına köpeğin ne olduğunu tanımlamıyor veya direkt
+köpeği algılamıyor sadece köpeği nesne olarak algılıyor ve onun konumunu işaretliyor.
+"""
+
+"""
+Nesne Tespitinde Sıralama
+-kenar algılama
+-köşe algılama
+-kontur algılama
+-renk ile nesne algılama
+-şablon eşleştirme
+-özellik eşleştirme
+-havza algoritması
+-yüz tanıma projesi
+-kedi yüzü tanımlama projesi
+-özel benzer özellikler ile nesne algılama
+-yaya algılama
+"""
